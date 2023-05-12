dd7c770e8931d5886e4c927a0988b935558bfd8f
ec50n9 <shelloworld@qq.com>
2023-05-13 03:00:40 +0800

    引入router和tailwindcss，完成首页动画

---
+++ src/route.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const Home = () => import("./views/Home.vue");
+const About = () => import("./views/About.vue");
+
+const routes = [
+  { path: "/", component: Home },
+  { path: "/about", component: About },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<script lang="js" setup></script>
+
+<template>
+  <div>
+    <h1>About</h1>
+  </div>
+</template>
+
+<style scoped></style>
